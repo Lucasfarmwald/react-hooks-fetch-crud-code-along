fd84e2cf644ce93d853566161857ad08a629f32f
lucasfarmwald <farmwaldlucas@gmail.com>
2022-02-13 23:13:53 -0600

    added patch request for item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 function Item({ item }) {
+
+  function handleAddToCartClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+    .then((r) => r.json())
+    .then((updatedItems) => console.log(updatedItems))
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button  onClick={handleAddToCartClick}  className={item.isInCart ? "remove" : "add"}>
+        
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
       <button className="remove">Delete</button>
